a21c176ffe5f9122a07c40bbf4aabddb4147f641
luckybir <luckybir@126.com>
2020-09-07 11:14:34 +0800

    simplify broswe tasK

--- taobao99.js
+++ taobao99.js
@@ -13,14 +13,16 @@ function task() {
   // result = taskCenter[66].click();
   // delay(3);
 
-  // executeTasks("浏览15秒");
-  executeTasks("立即前往得");
+  // 浏览15秒得星星，排除优酷
+  executeBrowseTasks();
 
-  //提醒我明日领红包(0/1)开启提醒获得50星星
-  //浏览U先，赢1分钱试用(0/1)立即前往得50星星
   //去玩淘金币小镇(0/1)立即前往得50星星
   //逛一逛淘宝人生(0/1)立即前往得50星星
   //玩省钱消消消赢福利(0/1)立即前往得50星星
+  //浏览U先，赢1分钱试用(0/1)立即前往得50星星
+  // executeGotoAPPTasks();
+
+  //提醒我明日领红包(0/1)开启提醒获得50星星
 }
 
 function delay(seconds) {
@@ -28,23 +30,53 @@ function delay(seconds) {
   sleep(randomSeconds);
 }
 
-function executeTasks(tasksName) {
-  tasks = textContains(tasksName).find();
-
-  log(tasks.size());
+function executeBrowseTasks() {
+  tasks = textContains("浏览15秒").find();
 
   tasks.forEach((task) => {
-    let taskText = task.parent().child(0).text();
-
-log(task.parent().children())
-
-    return 
+    // let taskText = task.parent().child(0).text();
+    let taskText = task.text();
 
     // youku task leave
     let reg = /优酷/g;
-    if (taskText.test(reg)) {
+    if (taskText.search(reg) != -1) {
       return;
     }
+
+    // get click time and already finish time
+    reg = /\d+(?=\))/g;
+    let clickTimes = taskText.match(reg)[0];
+
+    reg = /\d+(?=\/\d+\))/g;
+    let finishTimes = taskText.match(reg)[0];
+
+    log(taskText);
+
+    if (clickTimes - finishTimes > 0) {
+      for (let i = 0; i < clickTimes - finishTimes; i++) {
+        log("click " + i + " times");
+        task.click();
+        delay(5);
+        scrollDown();
+        delay(25);
+        back();
+        delay(3);
+      }
+
+      // get result
+      taskText = taskText = task.text();
+      log(taskText);
+    }
+  });
+
+  delay(3);
+}
+
+function executeGotoAPPTasks() {
+  tasks = textContains("立即前往得").find();
+
+  tasks.forEach((task) => {
+    let taskText = task.parent().child(0).text();
 
     // get click time
     reg = /\d+(?=\))/g;
